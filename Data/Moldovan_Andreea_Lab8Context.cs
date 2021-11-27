69f3182d99f54545d9b379df36684863de21ba81
Amalia Moldovan <amaliaandreea.moldovan@email.com>
2021-11-26 18:36:45 -0800

    Add project files.

---
+++ Data/Moldovan_Andreea_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Andreea_Lab8.Models;
+
+namespace Moldovan_Andreea_Lab8.Data
+{
+    public class Moldovan_Andreea_Lab8Context : DbContext
+    {
+        public Moldovan_Andreea_Lab8Context (DbContextOptions<Moldovan_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Moldovan_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Moldovan_Andreea_Lab8.Models.Category> Category { get; set; }
+    }
+}
